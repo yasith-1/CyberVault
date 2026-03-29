cabaae2184e42416c5f33d9a78e7039e0237d4ef
Yasith <141606159+yasith-1@users.noreply.github.com>
2026-03-30 00:23:11 +0530

    fix: add missing mongoose require in index.js for health check

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const connectDB = require("./config/db");
 const files = require("./routes/file.js");
